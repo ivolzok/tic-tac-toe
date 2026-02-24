02b786a785363fda178fe0940f6e3e67b21cd9aa
ivolzok <ivolzok@gmail.com>
2026-02-24 20:11:39 +0500

    last fixes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const container = document.getElementById('fieldWrapper');
 startGame();
 addResetListener();
 
-function startGame () {
+function startGame() {
     renderGrid(dimension);
     for (let i = 0; i < dimension; i++) {
         field[i] = [];
@@ -20,59 +20,59 @@ function startGame () {
     }
 }
 
-function CheckWinner(){
-    for (let row = 0; row < dimension; row++){
+function CheckWinner() {
+    for (let row = 0; row < dimension; row++) {
         startCell = field[row][0]
         win = true
-        for (let col = 1; col < dimension; col++){
-            if (field[row][col] !== startCell){
+        for (let col = 1; col < dimension; col++) {
+            if (field[row][col] !== startCell) {
                 win = false
                 break
             }
-        if (win)
-            return startCell
         }
+        if (win && startCell !== EMPTY)
+                return startCell
     }
 
-    for (let col = 0; col < dimension;col++){
+    for (let col = 0; col < dimension; col++) {
         startCell = field[0][col]
         win = true
-        for (let row = 1; row<dimension; row++){
-            if (field[row][col] !== startCell){
+        for (let row = 1; row < dimension; row++) {
+            if (field[row][col] !== startCell) {
                 win = false
                 break
             }
         }
-        if (win)
+        if (win && startCell !== EMPTY)
             return startCell
     }
 
     startCell = field[0][0]
     win = true
-    for (let i = 1; i<dimension; i++){
-        if (field[i][i] !== startCell){
+    for (let i = 1; i < dimension; i++) {
+        if (field[i][i] !== startCell) {
             win = false
             break
         }
     }
-    if (win)
-            return startCell
+    if (win && startCell !== EMPTY)
+        return startCell
 
     startCell = field[dimension - 1][0]
     win = true
-    for (let i = 0; i<dimension; i++){
-        if (field[dimension - 1 - i][i] !== startCell){
+    for (let i = 0; i < dimension; i++) {
+        if (field[dimension - 1 - i][i] !== startCell) {
             win = false
             break
         }
     }
-    if (win)
+    if (win && startCell !== EMPTY)
         return startCell
 
     return EMPTY
 }
 
-function renderGrid (dimension) {
+function renderGrid(dimension) {
     container.innerHTML = '';
 
     for (let i = 0; i < dimension; i++) {
@@ -87,16 +87,16 @@ function renderGrid (dimension) {
     }
 }
 
-function cellClickHandler (row, col) {
+function cellClickHandler(row, col) {
     console.log(`Clicked on cell: ${row}, ${col}`);
-    if(field[row][col] != EMPTY){
+    if (field[row][col] != EMPTY) {
         return;
     }
-    if (turn % 2 == 1){
+    if (turn % 2 == 1) {
         renderSymbolInCell(ZERO, row, col);
         field[row][col] = ZERO;
     }
-    else{
+    else {
         renderSymbolInCell(CROSS, row, col);
         field[row][col] = CROSS;
     }
@@ -105,34 +105,34 @@ function cellClickHandler (row, col) {
     if (winner != EMPTY)
         alert(winner)
 
-    if (turn == dimension * dimension){
+    if (turn == dimension * dimension) {
         alert("Победила Дружба!")
     }
 }
 
-function renderSymbolInCell (symbol, row, col, color = '#333') {
+function renderSymbolInCell(symbol, row, col, color = '#333') {
     const targetCell = findCell(row, col);
 
     targetCell.textContent = symbol;
     targetCell.style.color = color;
 }
 
-function findCell (row, col) {
+function findCell(row, col) {
     const targetRow = container.querySelectorAll('tr')[row];
     return targetRow.querySelectorAll('td')[col];
 }
 
-function addResetListener () {
+function addResetListener() {
     const resetButton = document.getElementById('reset');
     resetButton.addEventListener('click', resetClickHandler);
 }
 
-function resetClickHandler () {
+function resetClickHandler() {
     console.log('reset!');
     turn = 0;
     let dim = field.length;
-    for (let i = 0; i < dim; i++){
-        for(let j = 0; j < dim; j++){
+    for (let i = 0; i < dim; i++) {
+        for (let j = 0; j < dim; j++) {
             field[i][j] = EMPTY;
             renderSymbolInCell(EMPTY, i, j);
         }
@@ -142,7 +142,7 @@ function resetClickHandler () {
 
 /* Test Function */
 /* Победа первого игрока */
-function testWin () {
+function testWin() {
     clickOnCell(0, 2);
     clickOnCell(0, 0);
     clickOnCell(2, 0);
@@ -153,7 +153,7 @@ function testWin () {
 }
 
 /* Ничья */
-function testDraw () {
+function testDraw() {
     clickOnCell(2, 0);
     clickOnCell(1, 0);
     clickOnCell(1, 1);
@@ -166,6 +166,6 @@ function testDraw () {
     clickOnCell(2, 2);
 }
 
-function clickOnCell (row, col) {
+function clickOnCell(row, col) {
     findCell(row, col).click();
 }
